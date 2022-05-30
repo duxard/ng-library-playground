6fa8a6a57644a1182e1c37411709ac9e70fa4587
duxard <duxard@gmail.com>
2022-05-30 05:43:26 -0400

    Create library and testapp

---
+++ projects/my-lib/src/lib/components/button/button.component.ts
@@ -0,0 +1,20 @@
+import {Component, EventEmitter, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'lib-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css']
+})
+export class ButtonComponent implements OnInit {
+
+  buttonEmitter: EventEmitter<void> = new EventEmitter<void>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onBtnClick(): void {
+    this.buttonEmitter.emit();
+  }
+}
